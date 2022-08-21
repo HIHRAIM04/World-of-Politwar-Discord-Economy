5cc904668db6eb4fc041e8b47dda1e005c3a89ac
HIHRAIM <70520210+HIHRAIM04@users.noreply.github.com>
2022-08-21 16:12:05 +0300

    Create top+.js

---
+++ commands/top+.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const fs = require('fs')
+module.exports = {
+	name: 'top+',
+	description: 'Полный топ участников World of Politwar по деньгам.',
+    category: 'economy',
+	execute(message, args) {
+		var users = JSON.parse(fs.readFileSync("./users.json", "utf8"));
+        let usersMass = Object.entries(users);
+        usersMass = setTop(usersMass.sort(compareNumbers).slice(0, 100))
+        let topMess = new Discord.MessageEmbed()
+            .setTitle('Полный топ участников World of Politwar')
+            .setDescription(usersMass)
+            .setColor('RANDOM')
+        message.channel.send({embeds: [topMess]})
+        function setTop(arr) {
+            let text = ''
+            for(let a in arr) {
+                let b = parseInt(a) + 1
+                text += `\`${b}\`. <@${arr[a][0]}> - \`${arr[a][1].dollars}\`:dollar:\n`
+            }
+            return text
+        }
+        function compareNumbers(a, b) {
+            return b[1].dollars - a[1].dollars;
+        }
+	},
+};
